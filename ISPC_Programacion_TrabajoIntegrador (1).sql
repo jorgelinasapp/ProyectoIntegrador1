5e84e513b284815594fba91fc8841eb8315cd4b0
jorgelinasapp <126989877+jorgelinasapp@users.noreply.github.com>
2023-06-21 18:45:28 -0300

    Add files via upload

---
+++ ISPC_Programacion_TrabajoIntegrador (1).sql
@@ -0,0 +1,35 @@
+CREATE TABLE `Normativa`(
+    `id_Normativa` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `NroNormativa` INT NOT NULL,
+    `id_TipoNormativa` INT NOT NULL,
+    `Fecha` DATE NOT NULL,
+    `Descripcion` VARCHAR(255) NOT NULL,
+    `id_Categoria` INT NOT NULL,
+    `id_Jurisdiccion` INT NOT NULL,
+    `id_OrganoLegislativo` INT NOT NULL,
+    `PalabrasClave` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `Jurisdiccion`(
+    `id_Jurisdiccion` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Jurisdiccion` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `Categoria`(
+    `id_Categoria` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `TipoCategoria` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `OrganoLegislativo`(
+    `id_OrganoLegislativo` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `OrganoLegislativo` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `TipoNormativa`(
+    `id_TipoNormativa` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `TipoNormativa` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `Normativa` ADD CONSTRAINT `normativa_id_tiponormativa_foreign` FOREIGN KEY(`id_TipoNormativa`) REFERENCES `TipoNormativa`(`id_TipoNormativa`);
+ALTER TABLE
+    `Normativa` ADD CONSTRAINT `normativa_id_organolegislativo_foreign` FOREIGN KEY(`id_OrganoLegislativo`) REFERENCES `OrganoLegislativo`(`id_OrganoLegislativo`);
+ALTER TABLE
+    `Normativa` ADD CONSTRAINT `normativa_id_jurisdiccion_foreign` FOREIGN KEY(`id_Jurisdiccion`) REFERENCES `Jurisdiccion`(`id_Jurisdiccion`);
+ALTER TABLE
+    `Normativa` ADD CONSTRAINT `normativa_id_categoria_foreign` FOREIGN KEY(`id_Categoria`) REFERENCES `Categoria`(`id_Categoria`);
